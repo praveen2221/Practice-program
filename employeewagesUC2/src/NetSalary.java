cb676660d9e5b588ea437731a46f0a543ae1cacc
praveen2221 <praveenap2221@gmail.com>
2022-01-11 18:46:00 +0530

    UC2

---
+++ employeewagesUC2/src/NetSalary.java
@@ -0,0 +1,9 @@
+public class NetSalary {
+    public static void main(String[] arg)
+    {
+        double oneHourSalary=20;
+        double oneDayHour=8;
+        double netSalary=oneHourSalary*oneDayHour;
+        System.out.println("Net Salary is="+netSalary);
+    }
+}
